542d645bc12b93bfe2eb11653051e56cbc674f0b
Srikanth <ksrikanth@usaid.gov>
2024-11-18 18:18:21 -0500

    test push

---
+++ Scripts/test-push.R
@@ -0,0 +1,27 @@
+# AUTHOR:   K. Srikanth | USAID
+# PURPOSE:  
+# REF ID:   58cc7440 
+# LICENSE:  MIT
+# DATE:     2024-11-18
+# UPDATED: 
+
+# DEPENDENCIES ------------------------------------------------------------
+  
+  sipack
+
+# GLOBAL VARIABLES --------------------------------------------------------
+  
+  # SI specific paths/functions  
+    load_secrets()
+
+  # Grab metadata
+    get_metadata(filepath) 
+  
+  ref_id <- "58cc7440"
+
+# IMPORT ------------------------------------------------------------------
+  
+  return_data
+  msd
+
+# MUNGE -------------------------------------------------------------------
